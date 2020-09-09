6b3c4ccf305fc2a594b9169aeb4908264d98c19f
vagrant <vagrant@vagrant.vm>
2020-09-09 02:48:10 +0000

    added requirements; added jinja templating

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import FLask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect
 
 import os
-import requests
+# import requests
 
-app = Flask(_name_)
+#running an instance of flask
+app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRET"
 
 @app.route('/')
@@ -15,6 +16,6 @@ def homepage():
 
 
 if __name__ == '__main__':
-    app.debug = True
+    # app.debug = True
     # connect_to_db(app)
     app.run(port=5000, host='0.0.0.0')
